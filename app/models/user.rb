16af0d0181b6106c100ac6d06c974b6e04a8724d
luis <luiesh1x10@gmail.com>
2016-03-02 21:39:05 -0700

    modificaciones en la vistas y view port

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
- 	has_many :user_groups
-	has_many :groups, through: :user_groups
+ 	has_many :userGroups
+	has_many :groups, through: :userGroups
+	has_many :projects
+	has_many :projects, through: :userProjects
+	has_many :skills
+	has_many :experiences
   belongs_to :rank
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
